a486b6562c3f233731e3b103b195ed67f2f5bdd6
Kenneth Coffie <kenneth.coffie@evergreen-life.co.uk>
2021-05-14 16:50:31 +0100

    tried to fix robs windows issue

--- c-sharp/Program.cs
+++ c-sharp/Program.cs
@@ -1,22 +1,21 @@
 ﻿using System;
 using System.Text.Json;
 using System.Threading;
+using System.Threading.Tasks;
 using ElectronCgi.DotNet;
 using Microsoft.Extensions.Logging;
 
 
-class Message
-{
-  public string text { get; set; }
-  public string date { get; set; }
-  public int count { get; set; }
-}
+public record Message(string Text, string Date, int Count);
+
 
 namespace ElectronCgi
 {
   class Program
   {
-
+    static Connection connection = new ConnectionBuilder()
+                .WithLogging(minimumLogLevel: LogLevel.Trace, logFilePath: "cgi-log.txt")
+                .Build();
     static int number = 1;
     static string timeString = DateTime.Now.ToUniversalTime().ToString();
     static string time = DateTime.UtcNow.ToFileTime().ToString();
@@ -25,29 +24,26 @@ namespace ElectronCgi
     {
 
 
-      var connection = new ConnectionBuilder()
-                .WithLogging(minimumLogLevel: LogLevel.Trace, logFilePath: "cgi-log.txt")
-                .Build();
-
       connection.On<string, string>("greeting", (string name) =>
       {
-        var message = new Message();
-        message.text = $"Hello {name}!";
-        message.date = timeString;
-        message.count = number++;
+        var message = new Message($"Hello {name}", timeString, number);
 
         var json = JsonSerializer.Serialize(message);
 
         return json;
       });
 
-      new Timer(_ => connection.Send("emis-ping", $"Emis CSharp says {time}"), null, 0, 3000);
+      new Timer(_ => connection.Send<string>("emis-ping", $"Emis CSharp says {time}"), null, 0, 3000);
 
-
+      Test();
       connection.Listen();
 
     }
 
+    public static async void Test()
+    {
 
+      await Task.Delay(1000);
+    }
   }
 }
